4904987d1ad8269543a86118189d54ae22850974
Alex Bondarenko <bondarenko.v3@gmail.com>
2017-05-16 11:14:22 +0300

    add viewport

--- wp-content/themes/ukraineubc/header.php
+++ wp-content/themes/ukraineubc/header.php
@@ -3,6 +3,8 @@
 <head>
 	<meta http-equiv="Content-type" content="text/html; charset=<?php bloginfo('charset'); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="robots" content="noindex,nofollow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php wp_title('«', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<?php wp_head(); ?>
