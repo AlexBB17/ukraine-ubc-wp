c9984d6cf06dd26af82e88df4c7db841eefe29e8
Alex Bondarenko <bondarenko.v3@gmail.com>
2017-05-16 12:27:48 +0300

    modify background

--- wp-content/themes/ukraineubc/front-page.php
+++ wp-content/themes/ukraineubc/front-page.php
@@ -38,17 +38,17 @@
 			<h4>Наша команда</h4>
 			<div class="images row">
 				<div class="img-content">
-					<img class="img-circle" src="http://ukraine-ubc.kiev.ua/wp-content/uploads/2017/05/Sergey-Stupak-150x150.jpg" alt="">
+					<img class="img-circle" src="http://ukraine-ubc.kiev.ua/wp-content/uploads/2017/05/Sergey-Stupak.jpg" alt="">
 					<div class="employeer-name">Ступак Сергей</div>
 					<div class="employeer-description">Директор Ukraine-UBC</div>
 				</div>
 				<div class="img-content">
-					<img class="img-circle" src="http://ukraine-ubc.kiev.ua/wp-content/uploads/2017/05/Marina-Averina-1-150x150.jpg" alt="">
+					<img class="img-circle" src="http://ukraine-ubc.kiev.ua/wp-content/uploads/2017/05/Marina-Averina-1.jpg" alt="">
 					<div class="employeer-name">Аверина Марина</div>
 					<div class="employeer-description">Специалист Ukraine-UBC</div>
 				</div>
 				<div class="img-content">
-					<img class="img-circle" src="http://ukraine-ubc.kiev.ua/wp-content/uploads/2017/05/Lyudmila-CHerkass-150x150.jpg" alt="">
+					<img class="img-circle" src="http://ukraine-ubc.kiev.ua/wp-content/uploads/2017/05/Lyudmila-CHerkass.jpg" alt="">
 					<div class="employeer-name">Людмила Черкасс</div>
 					<div class="employeer-description">Специалист Ukraine-UBC</div>
 				</div>
